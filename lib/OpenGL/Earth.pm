67132ce05f198bda356774ca8c8c3412e27534f1
Cosimo Streppone <cosimo@cpan.org>
2009-01-28 07:34:30 +0000

    Import of COSIMO/OpenGL-Earth-0.01 from CPAN.
    gitpan-cpan-distribution: OpenGL-Earth
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         COSIMO/OpenGL-Earth-0.01.tar.bz2
    gitpan-cpan-author:       COSIMO
    gitpan-cpan-maturity:     released

---
+++ lib/OpenGL/Earth.pm
@@ -0,0 +1,68 @@
+# $Id: Earth.pm 132 2009-01-27 21:55:33Z Cosimo $
+
+package OpenGL::Earth;
+
+$VERSION = '0.01';
+
+use strict;
+use warnings;
+use OpenGL;
+
+# Some global variables
+
+# Window and texture IDs, window width and height.
+our $WINDOW_ID;
+
+our $WINDOW_WIDTH = 600;
+our $WINDOW_HEIGHT = 600;
+our @TEXTURES;
+our $WII;
+
+# Our display mode settings.
+our $LIGHT_ON     = 1;
+our $BLEND_ON     = 0;
+our $TEXTURE_ON   = 1;
+our $FILTERING_ON = 1;
+our $ALPHA_ADD    = 0;
+
+our $TEXTURE_MODE = GL_MODULATE;
+
+our @texture_mode = (GL_DECAL, GL_MODULATE, GL_BLEND, GL_REPLACE);
+our @texture_mode_str = qw(GL_DECAL GL_MODULATE GL_BLEND GL_REPLACE);
+
+1;
+
+=pod
+
+=head1 NAME
+
+OpenGL::Earth
+
+=head1 SYNOPSIS
+
+Mmh... I don't think you can use this module directly.
+Better look at the C<bin> folder.
+
+=head1 DESCRIPTION
+
+It's an attempt to write an OpenGL Perl program that can display
+a fancy rotating planet, while also displaying useful
+geographic information on it.
+
+The development is in a really really early stage now (Jan 2009).
+Don't expect miracles.
+
+It's basically a 10-years old OpenGL C program, translated
+to Perl like 8 years ago, and then butchered and reassembled
+by yours truly during some nightly hacking sessions.
+
+=head1 AUTHORS
+
+Cosimo Streppone, <cosimo@cpan.org>
+
+=head1 COPYRIGHT
+
+This code comes with the same terms as Perl itself.
+
+=cut
+
